2f2c42c60ca06d871216faf028ec89f44dc0cd09
pinchao <cpc4253@163.com>
2024-08-14 18:21:21 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ console.log(msg);
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://vuejs.org/" target="_blank">
+    <a v-if="" href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
